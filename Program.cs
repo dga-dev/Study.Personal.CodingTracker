184ced505e632de8a8103b366ac81d5dc1370469
dga-dev <76792258+dga-dev@users.noreply.github.com>
2024-03-10 02:13:45 +0100

    Keeping one message

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World with C# Academy! One more modification!");
-Console.WriteLine("Hello, World with C# Academy! One more modification!");
-Console.WriteLine("Hello, World with C# Academy! One more modification!");
-Console.WriteLine("Hello, World with C# Academy! One more modification!");
-Console.WriteLine("Hello, World with C# Academy! One more modification!");
-Console.WriteLine("Hello, World with C# Academy! One more modification!");
+
